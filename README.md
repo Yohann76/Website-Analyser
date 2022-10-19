89c1af76f0ed1df68bd0ad6b32d60d19ce41c3e0
Yohann Durand <yohanndurand76@gmail.com>
2022-10-19 23:12:45 +0200

    [Service] add first service for get URL in domain

---
+++ README.md
@@ -0,0 +1,30 @@
+# Website Analyzer 
+
+Website analyser is a project for monitoring website 
+
+for the moment this project is for 
+
+- Testing available URL 
+
+## Technology 
+
+- Symfony
+- Webpack Encore
+- Tailwind CSS 
+- MySQL 
+
+- Ansible
+- Docker 
+
+- Blackfire 
+
+
+
+# Later 
+
+- Cache (redis)
+... 
+
+# Deploy this project  
+
+# Start this project 
---
+++ src/Service/getAllUrlInDomain.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpClient\ScopingHttpClient;
+
+class getAllUrlInDomain
+{
+  
+    /**
+     * return all url for this domain 
+     */
+    public function getAllUrlInDomain($domain){
+
+        // return list url for this domain 
+
+    }
+
+    public function getOtherUrlInWebsite($file){
+
+        // return all other link in website 
+
+    }
+}
